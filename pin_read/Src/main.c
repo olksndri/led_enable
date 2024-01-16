7c754367a9a4ee4b4bcca2398ee0b438dbe379b4
Oleksandr Tymoshenko <118139643+olksndri@users.noreply.github.com>
2024-01-16 11:08:20 +0200

    Update main.c

--- pin_read/Src/main.c
+++ pin_read/Src/main.c
@@ -1,24 +1,19 @@
-
-#include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
+
+uint32_t volatile *const RCC_AHB1ENR		= (uint32_t*)(0x40023800 + 0x30);
+uint32_t volatile *const GPIOA_MODER 		= (uint32_t*)(0x40020000 + 0x00);
+uint32_t volatile *const GPIOC_MODER 		= (uint32_t*)(0x40020800 + 0x00);
+uint32_t volatile *const GPIOA_ODR 			= (uint32_t*)(0x40020000 + 0x14);
+uint32_t const volatile *const GPIOC_IDR 	= (uint32_t*)(0x40020800 + 0x10);
 
 int main(void) {
-
-	uint32_t *const GPIOA_AHB1_RCC = (uint32_t*)(0x40023800 + 0x30);
-	uint32_t *const GPIOA_PORT_MODE = (uint32_t*)(0x40020000 + 0x00);
-	uint32_t *const GPIOA_PORT_INPUT = (uint32_t*)(0x40020000 + 0x10);
-	uint32_t *const GPIOA_PORT_OUTPUT = (uint32_t*)(0x40020000 + 0x14);
-
-	*GPIOA_AHB1_RCC |= 1; // enable clock on AHB1 BUS (for GPIOA)
-	*GPIOA_PORT_MODE &= ~(3); // enable PA0 as input
-	*GPIOA_PORT_MODE |= (1 << 10); // enable PA5 as output
+	*RCC_AHB1ENR |= 5;
+	*GPIOA_MODER |= (1 << 10);
+	*GPIOC_MODER &= ~(3 << 26);
 
 	while(1) {
-		uint32_t input = *GPIOA_PORT_INPUT & 1;
-		if(input) {
-			*GPIOA_PORT_OUTPUT |= (1 << 5);
-		} else {
-			*GPIOA_PORT_OUTPUT &= ~(1 << 5);
-		}
+		if(!((*GPIOC_IDR >> 13) & 1)) *GPIOA_ODR |= (1 << 5);
+		else *GPIOA_ODR &= ~(1 << 5);
 	}
 }
